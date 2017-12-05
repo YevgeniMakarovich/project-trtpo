c2da1e770c405682faeb30a4c3696cfeef12ee7c
makhunov <apostal25031997@gmail.com>
2017-12-06 01:02:53 +0300

    add new function
    Adding authentication, changing standard fields, adding an empty button for searching

---
+++ ITsearch/ITsearch/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITsearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ ITsearch/ITsearch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITsearch.MvcApplication" Language="C#" %>
